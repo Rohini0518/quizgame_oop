184f4c1957ca44f0301aa9a48071b221f1d065f2
Rohini0518 <rohinipolina0518@gmail.com>
2024-08-03 18:22:15 +0530

    added finalscore and endline in main.py

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ quiz=QuizBrain(question_bank)
 
 while quiz.still_there_questions():
     quiz.next_question()
-       
+print("Heyy, you completed the quiz game")
+print(f"your final score is {quiz.score}/{len(question_bank)}")       
